f9addb17d16f5d1400d8ad3f8bb658bb3fcf23f6
Nagesh Danturti <nagesh.danturti@wingenium.com>
2024-02-19 16:20:11 +0100

    added QHULL enable on

--- scripts/docker/buildDockerImage.sh
+++ scripts/docker/buildDockerImage.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build -t ttk --build-arg paraview=5.11.0 --build-arg ttk=dev .
+docker buildx prune
+docker builder prune -af
+docker build -t ttk --build-arg paraview=5.11.0 --build-arg ttk=dev --progress=plain . 2> /tmp/docker-log.txt
+#docker build -t ttk --progress=plain . 2> /tmp/docker-log.txt
